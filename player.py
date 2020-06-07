d6d83ded4490a06f5957a3e7d210c92e45ed0a17
Vladimir Yashunsky <vladimir.yashunsky@gmail.com>
2020-06-07 12:39:13 +0300

    use dedicated player for each played note may be less efficiant, but allows to play multiple notes at once

--- player.py
+++ player.py
@@ -15,28 +15,18 @@ class Player(object):
     def __init__(self, notes, instrument, length, volume):
         super(Player, self).__init__()
 
-        pathes = [FILE_TEMPLATE.format(instrument=instrument,
-                                       note=note,
-                                       length=length,
-                                       volume=volume)
-                  for note in notes]
-
-        instance = vlc.Instance('--play-and-pause')
-        media_list = instance.media_list_new([instance.media_new_path(path)
-                                              for path in pathes])
-
-        self.vlc_player = instance.media_list_player_new()
-        self.vlc_player.set_media_list(media_list)
-
-        self.tracks = {note: track_id for (track_id, note) in enumerate(notes)}
+        self.pathes = {note: FILE_TEMPLATE.format(instrument=instrument,
+                                                  note=note,
+                                                  length=length,
+                                                  volume=volume)
+                       for note in notes}
 
     def play(self, note):
-        track_id = self.tracks.get(note)
-        if track_id is not None:
-            self.vlc_player.play_item_at_index(track_id)
+        local_player = vlc.MediaPlayer(self.pathes[note])
+        local_player.play()
 
     def stop(self):
-        self.vlc_player.stop()
+        pass
 
     def pause(self):
-        self.vlc_player.pause()
+        pass
